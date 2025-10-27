43e5ef6bb66c99296e0c481eea328b91d71bb5c0
Alex <alexthefloff@gmail.com>
2025-10-28 01:06:07 +0200

    a

--- encription/encrypton.py
+++ encription/encrypton.py
@@ -1,18 +1,18 @@
 from cryptography.fernet import Fernet
 
-print("is de(1) or encription(2) needed")
+print("is en(1) or decription(2) needed")
 cry = int(input())
 
 
 key = Fernet.generate_key()
 fernet = Fernet(key)
 
-if (cry == 2):
+if (cry == 1):
     message = input("what is the message ")
     encMessage = fernet.encrypt(message.encode())
     print(encMessage)
 
-elif (cry == 1):
+elif (cry == 2):
     message = input("what needs decrypting ")
     decMessage = fernet.decrypt(message).decode()
     print(decMessage)
